b49b9cba07f2059b6feee6560a11126074502558
Siva2webdev <kasu56158@gmail.com>
2024-03-01 14:29:42 +0530

    Store

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Body from './Components/Body';
+import './index.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
